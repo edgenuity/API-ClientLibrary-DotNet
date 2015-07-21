7d6e2e1e70e43c0f013cca55fbc86b3d637f8aba
apieniezny <andrew.pieniezny@gmail.com>
2015-07-21 09:56:05 -0400

    sync model files

---
+++ RicOneApi/Models/SifXpress/XOtherPersonIdType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RicOneApi.Models.SifXpress
+{
+    /// <summary>
+    /// XOtherPersonIdType
+    /// </summary>
+    public class XOtherPersonIdType
+    {
+        public XOtherPersonIdType()
+        {
+            type = null;
+            id = null;
+        }
+        public string type { get; set; }
+        public string id { get; set; }
+    }
+}
